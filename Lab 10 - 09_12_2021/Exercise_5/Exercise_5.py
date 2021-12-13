74019a8e07701a8698a44ea57a6aebd9d440a3a1
Simone000 <Simone_Albano000@hotmail.com>
2021-12-13 13:13:32 +0100

    add

--- Lab 10 - 09_12_2021/Exercise_5/Exercise_5.py
+++ Lab 10 - 09_12_2021/Exercise_5/Exercise_5.py
@@ -1,49 +1,76 @@
 # Cesar cipher
 
+import sys
+
 salt = "raffinato"
 
 # Generate a list with all the alphabet digits
-alphabeth = [chr(i) for i in range(122, 96 , -1)]
+alphabeth = [chr(i) for i in range(97, 123)]
 
 def generate_key(salt: str):
+    reversed_alphabeth = list(reversed(alphabeth))
     salt = str(salt)
     key = []
 
     for i in salt:
         key.append(i)
-
-    for i in alphabeth:
+    for i in reversed_alphabeth:
         if i not in key:
             key.append(i)
-
     return key
 
 def check_encrypt_decrypt(cypher: str):
-    for line in api_file:
-        if line[0] == "#":
+        if cypher[0] == "#":
             return "decrypt"
         else:
             return "encrypt"
 
-def encrypt(text: str, key: str):
+def encrypt(text: str, key: list):
     text = str(text)
-    key = str(key)
+    key = list(key)
     buffer = "#"
-    for i in text:
-        buffer.join(key[text.index(i)]) 
+
+    for val in text:
+        if val in alphabeth:
+            buffer += key[alphabeth.index(val)]
+        else:
+            buffer += val
     return buffer
 
 def decrypt(encrypted_text: str, key: str):
-    pass
+    encrypted_text = str(encrypted_text)
+    key = list(key)
+    buffer = ""
 
-with open("api_file.txt", "w") as api_file:
-    key = generate_key(salt)
+    for val in encrypted_text:
+        if val in alphabeth:
+            buffer += alphabeth[key.index(val)]
+        else:
+            buffer += val
+    return buffer[1:]
 
-    for line in api_file:
-        if check_encrypt_decrypt() == "encrypt":
-            encrypted_line = encrypt(line, key)
-            api_file.write(f"Line \"{line}\" is being encrypted: {encrypted_line}")
-        elif check_encrypt_decrypt() == "decrypt":
-            decrypted_line = decrypt(line, key)
-            api_file.write(f"Line \"{line}\" is being decrypted: {encrypted_line}")
-api_file.close()
+def main():
+    try:
+        api_file = open(f"{sys.path[0]}\\api_file.txt", "r")
+        
+        stream = ""
+
+        # Generate the key
+        key = generate_key(salt)
+
+        for line in api_file:
+            if check_encrypt_decrypt(line) == "encrypt":
+                stream += encrypt(line, key)
+            elif check_encrypt_decrypt(line) == "decrypt":
+                stream += decrypt(line, key)
+    finally:
+        api_file.close()
+
+    try:
+        api_file = open(f"{sys.path[0]}\\api_file.txt", "w")
+        api_file.write(stream)
+    finally:
+        api_file.close() 
+    return 0
+
+main()
